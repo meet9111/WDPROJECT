a8cfc3d26fec8a62c9c1ad3552ada5bad983878f
Meet Suthar <56501153+meet9111@users.noreply.github.com>
2021-10-04 09:47:40 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,4 @@
 <?php
-session_start();
-if($_SESSION["log"]!=true)
-{
-    header("location:index.php");
-}
-?>
-
-<?php
 
 $modelname = $_REQUEST['modelname'];
 
@@ -16,8 +8,5 @@ $sql = "delete from tblmobile where modelname='$modelname'";
 
 mysqli_query($conn,$sql);
 
-
-session_start();
-$_SESSION["log"]=false;
 header("location:index.php");
 ?>
